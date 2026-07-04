77a1406ef6abad176b40397d64ccbdc2fce5dec1
Priyank Jain <priyank.jain2@zecdata.com>
2026-07-04 19:22:54 +0530

    update

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "my-app-terraform-state"
     key          = "prod/terraform.tfstate"
-    region       = "ap-northeast-1"
+    region       = "ap-southeast-1"
     use_lockfile = true
     encrypt      = true
   }
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  default = "ap-northeast-1"
+  default = "ap-southeast-1"
 }
 
 variable "app_name" {
