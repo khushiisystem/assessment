e6774efcea5639819d0235b1908fd5d099f056c5
khushiisystem <chouhankhushii6260@gmail.com>
2026-07-04 13:35:16 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-app-terraform-state"
-    key            = "prod/terraform.tfstate"
-    region         = "ap-northeast-1"
-    use_lockfile   = true
-    encrypt        = true
+    bucket       = "my-app-terraform-state"
+    key          = "prod/terraform.tfstate"
+    region       = "ap-northeast-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
