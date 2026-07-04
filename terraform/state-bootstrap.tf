86c2d9acab82f756a03bbdbbcb0ff8d23b4d0cba
khushiisystem <chouhankhushii6260@gmail.com>
2026-07-04 19:48:59 +0530

    Update state-bootstrap.tf

--- terraform/state-bootstrap.tf
+++ terraform/state-bootstrap.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "my-app-terraform-state"
+  bucket        = "zecdata-terraform-state-prod"
   force_destroy = true
 }
 
@@ -7,16 +7,5 @@ resource "aws_s3_bucket_versioning" "state" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
     status = "Enabled"
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
   }
 }
