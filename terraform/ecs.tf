cb2f30c9f8d536b0acabade1749cb79bfc8ca328
Priyank Jain <priyank.jain2@zecdata.com>
2026-07-05 01:35:09 +0530

    ecs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -61,14 +61,20 @@ resource "aws_launch_template" "ecs_lt" {
   image_id      = "ami-07655e24561dca245"
   instance_type = "t3.micro"
 
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.ecs_sg.id]
+  }
+
   iam_instance_profile {
     name = aws_iam_instance_profile.ecs_profile.name
   }
 
   user_data = base64encode(<<-EOF
-    #!/bin/bash
-    echo ECS_CLUSTER=my-app-cluster >> /etc/ecs/ecs.config
-  EOF
+#!/bin/bash
+echo ECS_CLUSTER=my-app-cluster >> /etc/ecs/ecs.config
+echo ECS_ENABLE_TASK_IAM_ROLE=true >> /etc/ecs/ecs.config
+EOF
   )
 }
 
